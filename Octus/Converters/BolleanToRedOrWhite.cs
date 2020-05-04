16272fea86016473c6d019eaa09310b005f05e41
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2020-05-04 18:59:52 -0300

    WIP - Omplementando o tratamento para dias já agendados por outros clientes

---
+++ Octus/Converters/BolleanToRedOrWhite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Octus.Converters
+{
+    public class BolleanToRedOrWhite : IValueConverter
+    {
+        string corPositiva = "#a1edb5";
+        string corNegativa = "#f7665c";
+
+        public BolleanToRedOrWhite()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return ((bool)value) ? corNegativa : corPositiva;
+                
+
+            return corPositiva;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
